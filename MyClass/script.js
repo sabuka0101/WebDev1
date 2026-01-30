298b68e9cf6e655a5e80d974d58483c2390a4389
sabuka0101 <162179799+sabuka0101@users.noreply.github.com>
2026-01-30 14:05:50 +0400

    g

--- MyClass/script.js
+++ MyClass/script.js
@@ -1,10 +1,11 @@
 let num = prompt("Enter The Number");
 
-while (num !== 20) {
-    num = prompt("try again")
+while (num != 20) {
     if (num < 20) {
         alert("higher")
     } else if (num > 20) {
         alert("lower")
     }
+    num = prompt("Try again")
 }
+alert("You've guessed the number")

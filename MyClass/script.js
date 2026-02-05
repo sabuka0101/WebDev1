1fcfebaeec2f05ee3d29cc50c0f14e681829641d
sabuka0101 <162179799+sabuka0101@users.noreply.github.com>
2026-02-05 19:58:41 +0400

    g

--- MyClass/script.js
+++ MyClass/script.js
@@ -1,10 +1,12 @@
-let num = prompt("Enter The Number");
-
-while(num !== 20){
-    num = prompt("try again")
-    if(num<20){
-        alert("higher")
-    }else if(num>20){
-        alert("lower")
+const usernames = ["saba1","saba2","saba3"];
+const passwords = ["saba123","nika123","dato123"]
+let username = prompt("Enter your username")
+let password = prompt("Enter your password")
+for(let i = 0; i < usernames.length && passwords.length; i++){
+    if(username === usernames[i] && password === passwords[i]){
+     alert("Login successful")
+    }else{
+        alert("Invalid username or password")
     }
+    break;
 }

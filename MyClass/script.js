41d811367d729827023adf49695c22c84991bfdb
sabuka0101 <162179799+sabuka0101@users.noreply.github.com>
2026-02-14 17:28:43 +0400

    Update script.js

--- MyClass/script.js
+++ MyClass/script.js
@@ -1,73 +1,50 @@
-<<<<<<< HEAD
-const company = {
-    frontend: {
-        John: 1200,
-        Mary: 1500
-    },
-    backend: {
-        Peter: 1800,
-        Anna: 2000
-    },
-    design: {
-        Lucy: 1300
-    }
-};
-
-function analyzeCompany() {
-    let salary = 0;
-
-    for (department in company) {
-        for (employee in company[department]) {
-            salary += company[department][employee];
-        }
-    }
-}
-analyzeCompany();
-=======
-// let username = prompt("Enter Your Name:")
-// let age = prompt("Enter Your Age:")
-// let isCaptain = prompt("Are you captain? true/false")
-
-// const player = {
-//     username,
-//     age,
-//     isCaptain
-// }
-// if (isCaptain === "true"){
-//  alert(`This player is captain!. Name: ${username} and Age: ${age}`)
-// } else if(isCaptain === "false"){
-//     alert(`This is regular player. Name: ${username} and Age: ${age}`)
-// }else{
-//     alert(`Enter properties right`)
-// }
-
-// let game = {
-//     teamA: 0,
-//     teamB: 0
-// }
-// function addGoal(teamName){
-// if(teamName === "A"){
-//     game.teamA++;
-// }else if(teamName === "b"){
-//     game.teamB++;
-// }
-// console.log(game);
-// }
-
-// addGoal("A")
-// addGoal("B")
-
-let students = [
-  { name: "Nika", score: 80 },
-  { name: "Luka", score: 55 },
-  { name: "Ana", score: 92 },
+const products = [
+  {
+    name: "Headphones",
+    price: 120,
+    discount: 20,
+    inStock: true,
+  },
+  {
+    name: "Keyboard",
+    price: 80,
+    discount: 10,
+    inStock: false,
+  },
+  {
+    name: "Mouse",
+    price: 40,
+    discount: 5,
+    inStock: true,
+  },
+  {
+    name: "Monitor",
+    price: 300,
+    discount: 50,
+    inStock: true,
+  },
 ];
-
-for (i = 0; i < students.length; i++) {
-  if (students[i].score >= 60) {
-    console.log(`${students[i].name} passed`);
+for (i = 0; i < products.length; i++) {
+  if (products[i].inStock === true) {
+    console.log(
+      `${products[i].name} is available. Final price: ${products[i].price - (products[i].price * products[i].discount) / 100}`,
+    );
   } else {
-    console.log(`${students[i].name} failed`);
+    console.log(`${products[i].name} isn't available`);
   }
 }
->>>>>>> 68e844a4e68d9943586a86cbb04f013972527817
+
+//---------------------------------------------------
+
+const cart = {
+  user: "Ana",
+  items: ["Headphones", "Mouse", "Keyboard"],
+  prices: [120, 40, 80],
+};
+let total = 0;
+
+for (i = 0; i < cart.items.length; i++) {
+  total += cart.prices[i];
+  console.log(`${cart.user} is buying ${cart.items[i]}`);
+}
+console.log(`The total is ${total}$`);

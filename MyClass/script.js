1f1fe18e6d525af51be4d520d70d668d69f3fe84
sabuka0101 <162179799+sabuka0101@users.noreply.github.com>
2026-02-24 19:11:11 +0400

    g

--- MyClass/script.js
+++ MyClass/script.js
@@ -1,50 +1,21 @@
-const products = [
-  {
-    name: "Headphones",
-    price: 120,
-    discount: 20,
-    inStock: true,
+const students = {
+  firstName: "Nika",
+  lastName: "Gelashvili",
+  grades: [70, 50, 34],
+  getAverage(sum) {
+    for (let i = 0; i < this.grades.length; i++) {
+      sum += this.grades[i];
+    }
+    return sum / this.grades.length;
   },
-  {
-    name: "Keyboard",
-    price: 80,
-    discount: 10,
-    inStock: false,
+  isPassing() {
+    if (students.getAverage() > 50) {
+      return `${students.firstName} is passing`;
+    } else {
+      return `${students.firstName} isn't passing`;
+    }
   },
-  {
-    name: "Mouse",
-    price: 40,
-    discount: 5,
-    inStock: true,
-  },
-  {
-    name: "Monitor",
-    price: 300,
-    discount: 50,
-    inStock: true,
-  },
-];
-for (i = 0; i < products.length; i++) {
-  if (products[i].inStock === true) {
-    console.log(
-      `${products[i].name} is available. Final price: ${products[i].price - (products[i].price * products[i].discount) / 100}`,
-    );
-  } else {
-    console.log(`${products[i].name} isn't available`);
-  }
-}
-
-//---------------------------------------------------
-
-const cart = {
-  user: "Ana",
-  items: ["Headphones", "Mouse", "Keyboard"],
-  prices: [120, 40, 80],
 };
-let total = 0;
 
-for (i = 0; i < cart.items.length; i++) {
-  total += cart.prices[i];
-  console.log(`${cart.user} is buying ${cart.items[i]}`);
-}
-console.log(`The total is ${total}$`);
+console.log(students.getAverage(0));
+console.log(students.isPassing());

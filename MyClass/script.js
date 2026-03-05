f3926ca0c5e2f139d591af5fccc66892564eadff
sabuka0101 <162179799+sabuka0101@users.noreply.github.com>
2026-03-05 18:44:20 +0400

    g

--- MyClass/script.js
+++ MyClass/script.js
@@ -1,7 +1,16 @@
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const result = document.getElementById("result");
+const database = [
+  { username: "alice", password: "alice123" },
+  { username: "bob", password: "bob123" },
+  { username: "charlie", password: "charlie123" },
+];
 
-function sum() {
-  result.textContent = Number(num1.value) + Number(num2.value);
+function LogIn(event) {
+  event.preventDefault();
+  //   შეავსეთ ფუნქცია კოდით რომელიც ამოწმებს
+  //   იუზერნეიმი და პაროლი რომ არ იყოს ცარიელი
+  //   ასევე, ბაზაში (database array ზემოთ მოცემული) გვაქვს თუ არა მომხმარებელი მსგავსი იუზერით და პაროლით
+  //   თუ მომხმარებელი მოიძებნა, დაბეჭდეთ "Login successful." ალერტით
+  //   თუ იუზერი ცარიელი დაბეჭდეთ "Username cannot be empty." p ელემენტზე
+  //   თუ პაროლი ცარიელი დაბეჭდეთ "Password cannot be empty." p ელემენტზე
+  // თუ მომხმარებელი არ მოიძებნა დაბეჭდეთ "Incorrect username or password." p ელემენტზე
 }

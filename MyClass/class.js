722ed338abee5dbaca2fe7ae5fe47b330f4fc9dd
sabuka0101 <162179799+sabuka0101@users.noreply.github.com>
2026-03-21 16:56:18 +0400

    g

--- MyClass/class.js
+++ MyClass/class.js
@@ -1,13 +1,11 @@
 const btn = document.querySelector(".btn");
-const box = document.querySelector(".box");
-const top = document.querySelector(".top");
-const middle = document.querySelector(".middle");
-const bottom = document.querySelector(".bottom");
-const container = document.getElementById("main");
+const box = document.querySelectorAll(".box");
+const container = document.querySelector(".main");
 
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
-  box.classList.toggle("top");
-  box.classList.toggle("bottom");
-  box.classList.toggle("middle");
+  box[0].classList.toggle("top");
+  box[1].classList.toggle("middle");
+  box[2].classList.toggle("bottom");
+  container.classList.toggle("opened");
 });

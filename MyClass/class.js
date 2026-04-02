0da8efdfe08c22255ed4c7ec3190a8952dce35f5
sabuka0101 <147233725+123aleqsandre@users.noreply.github.com>
2026-04-02 20:07:18 +0400

    arrow functions

--- MyClass/class.js
+++ MyClass/class.js
@@ -1,11 +1,13 @@
-const btn = document.querySelector(".btn");
-const box = document.querySelectorAll(".box");
-const container = document.querySelector(".main");
+const numbers = [1, 2, 3, 4];
 
-btn.addEventListener("click", () => {
-  btn.classList.toggle("open");
-  box[0].classList.toggle("top");
-  box[1].classList.toggle("middle");
-  box[2].classList.toggle("bottom");
-  container.classList.toggle("opened");
-});
+const num = (x) => x * 2;
+
+function myMap(callback) {
+  let newNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    newNumbers.push(callback(numbers[i]));
+  }
+  return newNumbers;
+}
+
+console.log(myMap(num));

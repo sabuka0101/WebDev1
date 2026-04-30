272660835c11d13c5071033f9df5d5cbab6e2097
sabuka0101 <162179799+sabuka0101@users.noreply.github.com>
2026-04-30 18:36:02 +0400

    homework

--- Back/script.js
+++ Back/script.js
@@ -1,9 +1,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+const products = [
+  { id: 1, name: "mouse", price: 30 },
+  { id: 2, name: "keyboard", price: 20 },
+  { id: 3, name: "screen", price: 25 },
+];
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+app.get("/products/:id", (req, res) => {
+  console.log(req.params.id);
+  const parsedId = parseInt(req.params.id);
+  res.send(products[parsedId]);
 });
 
-app.listen(3000);
+app.set("view engine", "ejs");
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+app.post("/users", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(3000, () => {
+  console.log("app is listening at 3000 http://localhost:3000");
+});
